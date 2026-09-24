e66609b43d5f2616c2480fda651ef0f598e02828
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-24 07:15:47 +0100

    refactor: improve code and output readability
    Changed input array name to better communicate that the expected input
    is an array of numbers, where each number is a price representing a day.
    
    Output result to terminal in a nice way to improve readability.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,20 +1,20 @@
-def stock_picker(stock_prices) 
+def stock_picker(prices_per_day) 
   best_days = [0, 0]
   greatest_profit = 0
-  last_day = stock_prices.length - 1
+  last_day = prices_per_day.length - 1
 
-  stock_prices.each_with_index do |buy_price, buy_day|
-    last_day = stock_prices.length - 1
+  prices_per_day.each_with_index do |buy_price, buy_day|
+    last_day = prices_per_day.length - 1
 
     # relative to buy_day
-    possible_sell_days = stock_prices[buy_day.next..last_day] 
+    possible_sell_days = prices_per_day[buy_day.next..last_day] 
 
     possible_sell_days.each_with_index do |sell_price, sell_day|
       profit = sell_price - buy_price
       if profit > greatest_profit
         greatest_profit = profit
         best_days[0] = buy_day
-        # shift hack to get actual index ("day") in stock_prices
+        # shift hack to get actual index ("day") in `prices_per_day`
         best_days[1] = buy_day.next + sell_day 
       end
     end
@@ -23,6 +23,10 @@ def stock_picker(stock_prices)
   best_days
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
-# p stock_picker([1,5,3,8,6,2,7,4])
-# p stock_picker([3,5,3,1,9,6,12,2,7,1])
+# ARRAYS FOR TESTING
+some_days = [17,3,6,9,15,8,6,1,10]
+# some_other_days = [1,5,3,8,6,2,7,4]
+# some_other_other_days = [3,5,3,1,9,6,12,2,7,1]
+
+result = stock_picker(some_days)
+puts "Best day to buy: Day #{result[0]}\nBest day to sell: Day #{result[1]}"
